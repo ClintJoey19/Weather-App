c6dc3f52f2a45b49ab6e52cb29ac7b03a179158c
ClintJoey <clintjoey.llosala@cbsua.edu.ph>
2024-01-31 23:43:01 +0800

    commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiAOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "a5852ed83cmsh3a3e9a091cdc8c0p108441jsnd3f50eddbf81",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
